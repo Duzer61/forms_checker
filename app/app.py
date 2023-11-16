fec09bbb48b77fafcae43d4e88911fc412a52e9e
Duzer61 <dmi194@yandex.ru>
2023-11-16 15:02:56 +0300

    added match_finder

--- app/app.py
+++ app/app.py
@@ -50,3 +50,48 @@ def is_email(value):
     if re.match(EMAIL_REGEX, value):
         return True
     return False
+
+
+def validate_request_data(data):
+    """Определяет типы данных в полях запроса.
+    Проверяет на соответствие типам: date, phone, email.
+    Всем остальным полям присваивает тип string."""
+
+    for key, value in data.items():
+
+        if is_date(value):
+            data[key] = 'date'
+            continue
+
+        if is_phone_number(value):
+            data[key] = 'phone'
+            continue
+
+        if is_email(value):
+            data[key] = 'email'
+            continue
+
+        data[key] = 'string'
+
+    return data
+
+
+def find_match_forms(validated_data):
+    """Определяет совпадения полей запроса с полями шаблонов форм по их
+    названию и типу. Возвращает список с именами совпавших форм."""
+
+    match_forms = []
+    forms = collection.find({}, {"_id": 0})
+
+    for form in forms:
+        if all(
+            field_name in validated_data
+            and (
+                form[field_name] == validated_data[field_name]
+                or form[field_name] == 'string'
+            )
+            for field_name in form.keys()
+            if field_name != 'name'  # пропускает имена шаблонов форм
+        ):
+            match_forms.append(form['name'])
+    return sorted(match_forms)
