20fdbbfe86c200b8dc57cf15face3d805398baf3
Duzer61 <dmi194@yandex.ru>
2023-11-16 14:17:22 +0300

    add initial data

---
+++ app/initial_data.py
@@ -0,0 +1,79 @@
+# Начальные данные для заполнения БД
+
+forms_data = [
+    {
+        'name': 'User registration form',
+        'username': 'string',
+        'email': 'email',
+        'password': 'string',
+    },
+    {
+        'name': 'User login form',
+        'username': 'string',
+        'password': 'string',
+    },
+    {
+        'name': 'User personal data form',
+        'username': 'string',
+        'email': 'email',
+        'birthdate': 'date',
+        'phone': 'phone',
+        'address': 'string',
+        'biography': 'string'
+    },
+    {
+        'name': 'Just user phone form',
+        'username': 'string',
+        'phone': 'phone'
+    },
+    {
+        "name": "Message form",
+        "sender": "string",
+        "recipient": "string",
+        "message": "string",
+        "datestamp": "date"
+    },
+    {
+        'name': 'Historical personality form',
+        'person_name': 'string',
+        'birthdate': 'date',
+        'deathdate': 'date',
+        'biography': 'string'
+    },
+    {
+        'name': 'A kind of alien form',
+        'species': 'string',
+        'homeplanet': 'string',
+        'discovery_date': 'date',
+        'description': 'string'
+    },
+    {
+        'name': 'Alien-hunter registration form',
+        'huntername': 'string',
+        'homeplanet': 'string',
+        'birthdate': 'date',
+        'email': 'email',
+        'voice_communication': 'phone',
+        'starship': 'string'
+    },
+    {
+        'name': 'Starship form',
+        'model': 'string',
+        'manufacturer': 'string',
+        'production_date': 'date',
+        'description': 'string'
+    },
+    {
+        'name': 'Planet form',
+        'planet_name': 'string',
+        'system': 'string',
+        'discovery_date': 'date',
+        'description': 'string'
+    },
+    {
+        'name': 'Planet form 2',
+        'planet_name': 'string',
+        'system': 'string',
+        'description': 'string'
+    }
+]
