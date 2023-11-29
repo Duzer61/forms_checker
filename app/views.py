2332941f380e92485b7cbd2733d29e5c68b703f6
Duzer61 <dmi194@yandex.ru>
2023-11-29 18:32:29 +0300

    add utils, add post handler

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
-from flask import request
-from checker import find_match_forms
-from validators import validate_request_data
 from typing import Union
+
+from flask import request
+
+from checker import find_match_forms
+from utils import handle_post_request
+from validators import validate_request_data
 
 
 def get_form() -> Union[list, dict]:
@@ -14,7 +17,7 @@ def get_form() -> Union[list, dict]:
         Union[list, dict]: Имена совпавших форм или валидированные данные.
     """
 
-    request_data = dict(request.args)
+    request_data = handle_post_request(request)
     validated_data = validate_request_data(request_data)
     match_forms_names = find_match_forms(validated_data)
     if match_forms_names:
